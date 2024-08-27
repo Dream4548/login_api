d0312c0a08427e30303c6860b6391fb78724ebcd
Dream4548 <deemgamer123@gmail.com>
2024-08-28 01:25:31 +0700

    infoandedit

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,11 @@ import 'dart:convert';
 import 'package:google_fonts/google_fonts.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
-void main() {
+void main() async {
+  // ตรวจสอบให้แน่ใจว่า Flutter binding ถูกติดตั้ง
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
@@ -47,11 +50,19 @@ class _LoginPageState extends State<LoginPage> {
       if (response.statusCode == 200) {
         final responseData = json.decode(response.body);
         final token = responseData['token'];
-        await _saveTokenToFile(token);
+
+        // แสดง token ใน debug console
+        print('Login successful. Token: $token');
+
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Login successful')),
         );
-        // TODO: Navigate to the next screen or perform other actions
+
+        // Navigate to UserInformationPage
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => UserInformationPage()),
+        );
       } else if (response.statusCode == 401) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Invalid username or password')),
@@ -65,17 +76,6 @@ class _LoginPageState extends State<LoginPage> {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('An error occurred. Please try again later.')),
       );
-    }
-  }
-
-  Future<void> _saveTokenToFile(String token) async {
-    try {
-      final directory = await getApplicationDocumentsDirectory();
-      final file = File('${directory.path}/token.txt');
-      await file.writeAsString(token);
-      print('Token saved successfully');
-    } catch (e) {
-      print('Error saving token: $e');
     }
   }
 
@@ -227,6 +227,278 @@ class _SignUpPageState extends State<SignUpPage> {
                 Navigator.pop(context);
               },
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//UserInterform
+class UserInformationPage extends StatefulWidget {
+  @override
+  _UserInformationPageState createState() => _UserInformationPageState();
+}
+
+class _UserInformationPageState extends State<UserInformationPage> {
+  Map<String, dynamic> _userInfo = {};
+  bool _isLoading = true;
+  String _error = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserInformation();
+  }
+
+  Future<void> _fetchUserInformation() async {
+    try {
+      final token = await _getToken();
+      final response = await http.get(
+        Uri.parse('https://wallet-api-7m1z.onrender.com/user/information'),
+        headers: {'Authorization': 'Bearer $token'},
+      );
+
+      if (response.statusCode == 200) {
+        setState(() {
+          _userInfo = json.decode(response.body);
+          _isLoading = false;
+        });
+      } else if (response.statusCode == 401) {
+        setState(() {
+          _error = 'Unauthorized: Invalid or missing token.';
+          _isLoading = false;
+        });
+      } else {
+        setState(() {
+          _error = 'Failed to load user information.';
+          _isLoading = false;
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _error = 'An error occurred: $e';
+        _isLoading = false;
+      });
+    }
+  }
+
+  Future<String> _getToken() async {
+    try {
+      return await rootBundle.loadString('assets/token.txt');
+    } catch (e) {
+      throw Exception('Failed to load token: $e');
+    }
+  }
+
+  Widget _buildInfoItem(String label, String value) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            label,
+            style: GoogleFonts.roboto(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            value,
+            style: GoogleFonts.roboto(fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'User Information',
+          style: GoogleFonts.roboto(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : _error.isNotEmpty
+              ? Center(
+                  child: Text(
+                    _error,
+                    style: GoogleFonts.roboto(
+                      color: Colors.red,
+                      fontSize: 16,
+                    ),
+                  ),
+                )
+              : Center(
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blue, width: 2),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'User Details',
+                          style: GoogleFonts.roboto(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        _buildInfoItem('User ID', _userInfo['userId'] ?? ''),
+                        _buildInfoItem('Username', _userInfo['username'] ?? ''),
+                        _buildInfoItem('First Name', _userInfo['fname'] ?? ''),
+                        _buildInfoItem('Last Name', _userInfo['lname'] ?? ''),
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () async {
+                            final result = await Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => EditUserNamePage(
+                                  currentFname: _userInfo['fname'] ?? '',
+                                  currentLname: _userInfo['lname'] ?? '',
+                                ),
+                              ),
+                            );
+
+                            if (result == true) {
+                              _fetchUserInformation(); // อัพเดตข้อมูลหลังการแก้ไข
+                            }
+                          },
+                          child: Text('Edit Profile'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+    );
+  }
+}
+
+//eidt
+class EditUserNamePage extends StatefulWidget {
+  final String currentFname;
+  final String currentLname;
+
+  EditUserNamePage({required this.currentFname, required this.currentLname});
+
+  @override
+  _EditUserNamePageState createState() => _EditUserNamePageState();
+}
+
+class _EditUserNamePageState extends State<EditUserNamePage> {
+  final TextEditingController _fnameController = TextEditingController();
+  final TextEditingController _lnameController = TextEditingController();
+  bool _isLoading = false;
+  String _error = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _fnameController.text = widget.currentFname;
+    _lnameController.text = widget.currentLname;
+  }
+
+  Future<void> _updateUserProfile() async {
+    setState(() {
+      _isLoading = true;
+      _error = '';
+    });
+
+    final token = await _getToken();
+    final response = await http.post(
+      Uri.parse('https://wallet-api-7m1z.onrender.com/user/set/profile'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({
+        'fname': _fnameController.text,
+        'lname': _lnameController.text,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      Navigator.of(context).pop(true); // ส่งสัญญาณกลับเมื่อทำสำเร็จ
+    } else {
+      setState(() {
+        _error =
+            json.decode(response.body)['error'] ?? 'Failed to update profile.';
+        _isLoading = false;
+      });
+    }
+  }
+
+  Future<void> _deleteUserName() async {
+    setState(() {
+      _fnameController.clear();
+      _lnameController.clear();
+    });
+  }
+
+  Future<String> _getToken() async {
+    try {
+      return await rootBundle.loadString('assets/token.txt');
+    } catch (e) {
+      throw Exception('Failed to load token: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Profile'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _fnameController,
+              decoration: InputDecoration(labelText: 'First Name'),
+            ),
+            TextField(
+              controller: _lnameController,
+              decoration: InputDecoration(labelText: 'Last Name'),
+            ),
+            SizedBox(height: 20),
+            _isLoading
+                ? CircularProgressIndicator()
+                : Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: _updateUserProfile,
+                        child: Text('Save Changes'),
+                      ),
+                      SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: _deleteUserName,
+                        child: Text('Clear Names'),
+                        style: ElevatedButton.styleFrom(),
+                      ),
+                    ],
+                  ),
+            if (_error.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Text(
+                  _error,
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
           ],
         ),
       ),
